9310bb2ed07aaedb0ce3722283c3643b6799131b
Fingerlingus <sirfingerlingus@gmail.com>
2022-08-09 11:47:28 -0600

    move G_ALLOW_MATURE inside lazy_static! block, add SYS_CMDLINE global to store program's command line in, fetch command line in main, add calls to pmem::init() and dvar::init()

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![allow(non_snake_case, clippy::bool_comparison)]
 
-use std::sync::atomic::AtomicBool;
+use lazy_static::lazy_static;
+use std::sync::{atomic::AtomicBool, Arc, RwLock};
 
 mod cmd;
 mod com;
@@ -12,22 +13,36 @@ mod platform;
 mod pmem;
 mod sys;
 
-#[allow(dead_code)]
-static G_ALLOW_MATURE: AtomicBool = AtomicBool::new(true);
+lazy_static! {
+    #[allow(dead_code)]
+    static ref G_ALLOW_MATURE: AtomicBool = AtomicBool::new(true);
+    static ref SYS_CMDLINE: Arc<RwLock<String>> = Arc::new(RwLock::new(String::new()));
+}
 
 fn main() {
     platform::os::target::main();
+
+    let mut cmd_line: String = String::new();
+    for arg in std::env::args() {
+        cmd_line.push_str(&format!("{} ", &arg));
+    }
+    cmd_line = cmd_line.trim().to_string();
+
+    {
+        let lock = SYS_CMDLINE.clone();
+        let mut writer = lock.write().unwrap();
+        writer.clear();
+        writer.insert_str(0, &cmd_line);
+    }
+
+    pmem::init();
+    dvar::init();
     dvar::register_string(
         "test".to_string(),
         "abcd".to_string(),
         dvar::DvarFlags::empty(),
         "Testing 123...".to_string(),
     );
-    let d = dvar::DVARS
-        .read()
-        .unwrap()
-        .get(&"test".to_string())
-        .unwrap()
-        .clone();
+    let d = dvar::find("test".to_string()).unwrap();
     com::println(format!("{}", d));
 }
