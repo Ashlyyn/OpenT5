2388b97aeb28ca0b8c36e5157193084b9e4ee7a7
Fingerlingus <sirfingerlingus@gmail.com>
2022-08-09 13:06:51 -0600

    add comments

--- src/com.rs
+++ src/com.rs
@@ -4,6 +4,8 @@ use lazy_static::lazy_static;
 use std::fs::File;
 use std::sync::{Arc, RwLock};
 
+// Needs to be actually implemented
+// Just a wrapper around print! currently
 pub fn print(message: String) {
     print!("{}", message);
 }
@@ -21,10 +23,13 @@ lazy_static! {
         Arc::new(RwLock::new(None));
 }
 
+// Check if log file is open
 pub fn log_file_open() -> bool {
     return LOG_FILE.clone().read().unwrap().is_some();
 }
 
+// Also needs to be actually implemented
+// Currently just a wrapper for panic
 pub fn error(err: String) {
     panic!("{}", err);
 }
@@ -33,6 +38,8 @@ pub fn errorln(err: String) {
     error(format!("{}\n", err));
 }
 
+// Implement these two later
+// (not integral to the program, look annoying to implement)
 #[allow(unused_variables, unreachable_code)]
 pub fn filter(string: String, name: String, case_sensitive: bool) -> bool {
     return true;
@@ -40,6 +47,9 @@ pub fn filter(string: String, name: String, case_sensitive: bool) -> bool {
     todo!("com::filter");
 }
 
-pub fn dvar_dump(_channel: i32, _param_2: String) {
+#[allow(unused_variables, unreachable_code)]
+pub fn dvar_dump(channel: i32, param_2: String) {
+    return;
+
     todo!("com::dvar_dump");
 }
