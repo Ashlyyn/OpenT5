427e61d370c9c45e45de213fc41c4db011a6055b
Fingerlingus <sirfingerlingus@gmail.com>
2022-08-11 11:59:41 -0600

    add get_localization_lang()

--- src/locale.rs
+++ src/locale.rs
@@ -17,6 +17,30 @@ enum Language {
     CZECH,
 }
 
-fn init() -> Language {
-    Language::ENGLISH
+fn get_localization_lang() -> Language {
+    let s = match std::fs::read("localization.txt") {
+        Err(_) => return Language::ENGLISH,
+        Ok(v) => match String::from_utf8(v) {
+            Err(_) => return Language::ENGLISH,
+            Ok(s) => s,
+        },
+    };
+
+    let lang_str = s.split('\n').collect::<Vec<&str>>()[0];
+    match lang_str {
+        "english" => Language::ENGLISH,
+        "french" => Language::FRENCH,
+        "frenchcanadian" => Language::FRENCHCANADIAN,
+        "german" => Language::GERMAN,
+        "austrian" => Language::AUSTRIAN,
+        "italian" => Language::ITALIAN,
+        "spanish" => Language::SPANISH,
+        "british" => Language::BRITISH,
+        "russian" => Language::RUSSIAN,
+        "polish" => Language::POLISH,
+        "korean" => Language::KOREAN,
+        "japanese" => Language::JAPANESE,
+        "czech" => Language::CZECH,
+        _ => Language::ENGLISH,
+    }
 }
