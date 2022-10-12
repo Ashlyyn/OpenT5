f9e118fe51e99e98e5a6d88f6b203f3a9650cda9
Fingerlingus <sirfingerlingus@gmail.com>
2022-10-12 05:13:49 -0600

    add PlayerKeyState and requisite structs

--- src/key.rs
+++ src/key.rs
@@ -1,4 +1,57 @@
-#[allow(unused_variables)]
+#![allow(dead_code)]
+
+use std::sync::{Arc, RwLock};
+
+use arrayvec::ArrayVec;
+use lazy_static::lazy_static;
+
+#[derive(Copy, Clone, Default)]
+enum LocSelInputState {
+    #[default]
+    None,
+    Confirm,
+    Yaw,
+    Regroup,
+    Defend,
+    SquadCancel,
+    Cancel,
+}
+
+#[derive(Clone, Default)]
+struct KeyState {
+    down: bool,
+    repeats: i32,
+    binding: String,
+    binding2: String,
+}
+
+#[derive(Clone, Default)]
+struct Field {
+    cursor: i32,
+    scroll: i32,
+    draw_width: i32,
+    width_in_pixels: i32,
+    char_height: f32,
+    fixed_size: i32,
+    buffer: Vec<u8>,
+}
+
+#[derive(Clone, Default)]
+struct PlayerKeyState {
+    char_field: Field,
+    char_team: i32,
+    overstrike_mode: i32,
+    any_key_down: i32,
+    keys: ArrayVec<KeyState, 256>,
+    loc_sel_input_state: LocSelInputState,
+}
+
+lazy_static! {
+    static ref PLAYER_KEYS: Arc<RwLock<PlayerKeyState>> =
+        Arc::new(RwLock::new(Default::default()));
+}
+
 pub fn clear_states(ids: isize) {
-    todo!("clear_states");
+    PLAYER_KEYS.clone().try_write().expect("").any_key_down = 0;
+    todo!("key::clear_states");
 }
