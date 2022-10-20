668a82a09733b426b0e88af9e037078ade8e5fac
Fingerlingus <sirfingerlingus@gmail.com>
2022-10-19 20:43:43 -0600

    add Config and associated functions

--- src/vid.rs
+++ src/vid.rs
@@ -1,13 +1,44 @@
+use std::sync::RwLock;
+
 use crate::*;
 
+use lazy_static::lazy_static;
+
+#[derive(Copy, Clone, Default)]
+pub struct Config {
+    pub scene_width: u16,
+    pub scene_height: u16,
+    pub display_width: u16,
+    pub display_height: u16,
+    pub output_display_width: u16,
+    pub output_display_height: u16,
+    pub display_frequency: u16,
+    pub is_tool_mode: bool,
+    pub is_fullscreen: bool,
+    pub aspect_ratio_window: f32,
+    pub aspect_ratio_scene_pixel: f32,
+    pub aspect_ratio_display_pixel: f32,
+    pub max_texture_size: usize,
+    pub max_texture_maps: usize,
+    pub device_supports_gamma: bool,
+}
+
+lazy_static! {
+    pub static ref CONFIG: Arc<RwLock<Config>> =
+        Arc::new(RwLock::new(Default::default()));
+}
+
+pub fn config() -> Config {
+    *CONFIG.clone().try_read().expect("")
+}
+
 pub fn app_activate(active_app: bool, is_minimized: bool) {
+    key::clear_states(0);
     if is_minimized {
         platform::set_minimized();
     } else {
         platform::clear_minimized();
     }
-
-    println!("TODO: key::clear_states");
 
     if active_app == false {
         platform::clear_active_app();
@@ -16,5 +47,6 @@ pub fn app_activate(active_app: bool, is_minimized: bool) {
         println!("TODO: com::touch_memory");
     }
 
+    input::activate(active_app);
     // _DAT_027706dc = 0;
 }
