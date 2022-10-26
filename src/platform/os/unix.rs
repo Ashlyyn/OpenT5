af440fc51f13ce6dc7de0600928ccf242d4f0054
Fingerlingus <sirfingerlingus@gmail.com>
2022-10-26 04:39:23 -0600

    init gtk4 in unix main()

--- src/platform/os/unix.rs
+++ src/platform/os/unix.rs
@@ -1,6 +1,7 @@
-// This file is for any Unix-specific initialization that 
+// This file is for any Unix-specific initialization that
 // should be done before the rest of main() executes
 
 pub fn main() {
+    gtk4::init().unwrap();
     println!("Exiting Unix main()!");
 }
