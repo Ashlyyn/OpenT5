d7a1a26ecce1eca90e08f1de29b9d06c929eace0
Fingerlingus <sirfingerlingus@gmail.com>
2022-10-26 22:15:45 -0600

    add 'i' parameter of add_text to println!()

--- src/cbuf.rs
+++ src/cbuf.rs
@@ -1,5 +1,5 @@
-pub fn add_text(_: i32, text: &str) {
-    println!("cbuf::add_text: {}", text);
+pub fn add_text(i: i32, text: &str) {
+    println!("cbuf::add_text: ({}) {}", i, text);
 }
 
 pub fn add_textln(i: i32, text: &str) {
