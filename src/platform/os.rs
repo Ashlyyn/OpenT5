b55c73efd36a1b1a581413f6ff8c9601ce1013c6
Fingerlingus <sirfingerlingus@gmail.com>
2023-02-23 19:50:14 -0700

    modify to allow different execution paths for different Unix-like OSes instead of one monolithic Unix main

--- src/platform/os.rs
+++ src/platform/os.rs
@@ -1,17 +1,14 @@
 // This file exports the platform-specific modules as module "target"
 // to allow for easy execution in main()
 
-#[cfg(target_os = "windows")]
-pub mod win32;
-#[cfg(target_os = "windows")]
-pub use win32 as target;
+use cfg_if::cfg_if;
 
-#[cfg(target_os = "unix")]
-pub mod unix;
-#[cfg(target_os = "unix")]
-pub use unix as target;
-
-#[cfg(target_os = "linux")]
-pub mod unix;
-#[cfg(target_os = "linux")]
-pub use unix as target;
+cfg_if! {
+    if #[cfg(target_family = "windows")] {
+        pub mod win32;
+        pub use win32 as target;
+    } else if #[cfg(target_family = "unix")] {
+        pub mod unix;
+        pub use unix as target;
+    }
+}
--- src/platform/os/unix.rs
+++ src/platform/os/unix.rs
@@ -1,7 +1,19 @@
 // This file is for any Unix-specific initialization that
 // should be done before the rest of main() executes
 
+use cfg_if::cfg_if;
+
+cfg_if! {
+    if #[cfg(target_os = "linux")] {
+        pub mod linux;
+        pub use linux as target;
+    } else if #[cfg(target_os = "macos")] {
+        pub mod macos;
+        pub use macos as target;
+    }
+}
+
 pub fn main() {
-    gtk4::init().unwrap();
+    target::main();
     println!("Exiting Unix main()!");
 }
