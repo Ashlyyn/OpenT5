7367b6db1b2e23dc1bc81481deeda8d1cbc2b273
Fingerlingus <sirfingerlingus@gmail.com>
2023-03-06 22:12:52 -0700

    create so that platform::arch::target::main() exists to do wasm-specific initialization

---
+++ src/platform/arch/wasm.rs
@@ -0,0 +1,15 @@
+use cfg_if::cfg_if;
+
+cfg_if! {
+    if #[cfg(target_arch = "wasm32")] {
+        pub mod wasm32;
+        pub use wasm32 as target;
+    } else if #[cfg(target_arch = "wasm64")] {
+        pub mod wasm64;
+        pub use wasm64 as target;
+    }
+}
+
+pub fn main() {
+    target::main();
+}
