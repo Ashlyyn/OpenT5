0d7b04d2320bcc9bd74c0bee74cd41e825df1cb0
Fingerlingus <sirfingerlingus@gmail.com>
2023-03-06 17:54:17 -0700

    implement update_use_held and update_use_count

--- src/input.rs
+++ src/input.rs
@@ -47,3 +47,28 @@ fn is_foreground_window() -> bool {
 pub const fn shutdown() {
     mouse::deactivate();
 }
+
+pub fn update_use_held() {
+    let mut cgs = cl::get_local_client_globals_mut();
+    let cg = cgs.iter_mut().nth(0).unwrap();
+    if cg.use_held == false {
+        if dvar::get_int("cl_dblTapMaxDelayTime").unwrap() <= com::frame_time() as i32 - cg.use_time {
+            cg.use_count = 0;
+        }
+        cg.use_held = true;
+        cg.use_time = com::frame_time() as _;
+    }
+}
+
+pub fn update_use_count() {
+    let mut cgs = cl::get_local_client_globals_mut();
+    let cg = cgs.iter_mut().nth(0).unwrap();
+
+    if (com::frame_time() - cg.use_time as isize) < (dvar::get_int("cl_dblTapMaxDelayTime").unwrap() as isize) {
+        cg.use_count += 1;
+        cg.use_held = false;
+    } else {
+        cg.use_count = 0;
+        cg.use_held = false;
+    }
+}
