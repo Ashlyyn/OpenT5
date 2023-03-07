7c1add242281fd77c89ae3de1150bb8a7dfa03dc
Fingerlingus <sirfingerlingus@gmail.com>
2023-03-06 22:16:27 -0700

    add main() that calls target::main()

--- src/platform/arch.rs
+++ src/platform/arch.rs
@@ -5,5 +5,15 @@ cfg_if! {
     if #[cfg(any(target_arch = "x86", target_arch = "x86_64"))] {
         pub mod x86;
         pub use x86 as target;
+    } else if #[cfg(any(target_arch = "wasm32", target_arch = "wasm64"))] {
+        pub mod wasm;
+        pub use wasm as target;
+    } else {
+        pub mod other;
+        pub use other as target;
     }
+}
+
+pub fn main() {
+    target::main();
 }
--- src/platform/os.rs
+++ src/platform/os.rs
@@ -12,5 +12,15 @@ cfg_if! {
     } else if #[cfg(target_family = "unix")] {
         pub mod unix;
         pub use unix as target;
+    } else if #[cfg(target_arch = "wasm32")] {
+        pub mod none;
+        pub use none as target;
+    } else {
+        pub mod other;
+        pub use other as target;
     }
+}
+
+pub fn main() {
+    target::main();
 }
