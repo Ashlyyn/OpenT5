f96b1a10981060fe3e0f54e61bd1129b1c4a6939
Fingerlingus <sirfingerlingus@gmail.com>
2023-05-20 18:11:26 -0600

    fix imports on windows

--- src/sys.rs
+++ src/sys.rs
@@ -6,7 +6,7 @@ use crate::{
     platform::{
         WindowHandle,
     },
-    util::{EasierAtomic, EasierAtomicBool, SignalState, SmpEvent},
+    util::{EasierAtomicBool, SignalState, SmpEvent},
     *,
 };
 use num_derive::FromPrimitive;
@@ -173,14 +173,14 @@ pub fn milliseconds() -> isize {
     if BASE_TIME_ACQUIRED.load(SeqCst) == false {
         let now = SystemTime::now();
         let time = now
-            .duration_since(SystemTime::UNIX_EPOCH)
+            .duration_since(UNIX_EPOCH)
             .unwrap()
             .as_millis();
         TIME_BASE.store(time.try_into().unwrap(), SeqCst);
     }
 
     let time: isize = SystemTime::now()
-        .duration_since(SystemTime::UNIX_EPOCH)
+        .duration_since(UNIX_EPOCH)
         .unwrap()
         .as_millis()
         .try_into()
