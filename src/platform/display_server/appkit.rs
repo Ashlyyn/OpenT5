9e06a261e50abc0e1f19a1c765e7fd44fe012c58
Ashlyn <ashbtw19937@gmail.com>
2023-08-02 21:43:29 -0700

    appkit: fix key code translation, implement translation of system events (still need to do keyboard/mouse events)

--- src/platform/display_server/appkit.rs
+++ src/platform/display_server/appkit.rs
@@ -1,27 +1,34 @@
 // macOS can use AppKit with the `macos_use_appkit` feature enabled. Otherwise
 // we default to Xlib.
 
+#![allow(non_upper_case_globals)]
+
 #[link(name = "Cocoa", kind = "framework")]
 extern "C" {}
 
-use core::ptr::NonNull;
+use core::ptr::{addr_of_mut, NonNull};
+use std::ptr::addr_of;
 
 use icrate::{
     AppKit::{
-        NSApplication, NSApplicationDelegate, NSApplicationTerminateReply,
-        NSResponder, NSWindowDelegate, NSEvent,
+        NSApp, NSApplication, NSApplicationDelegate,
+        NSApplicationTerminateReply, NSEvent, NSEventTypeApplicationDefined,
+        NSResponder, NSWindowDelegate, NSWindow, NSBitsPerPixelFromDepth,
     },
-    Foundation::NSNotification,
+    Foundation::{CGPoint, NSDate, NSNotification, NSSize},
 };
 use objc2::{
-    declare_class, msg_send, msg_send_id,
+    declare::{Ivar, IvarEncode},
+    declare_class,
+    ffi::NSInteger,
+    msg_send, msg_send_id,
     mutability::InteriorMutable,
     rc::Id,
     runtime::{NSObject, NSObjectProtocol},
     ClassType,
 };
 
-use crate::sys::{self, WindowEvent, KeyboardScancode};
+use crate::sys::{KeyboardScancode, WindowEvent, self};
 
 pub fn init() {}
 
@@ -56,6 +63,7 @@ declare_class!(
             &self,
             _sender: &NSApplication,
         ) -> NSApplicationTerminateReply {
+            sys::set_quit_event();
             0
         }
     }
@@ -68,8 +76,11 @@ impl AppDelegate {
 }
 
 declare_class!(
-    #[derive(Debug)]
-    pub struct WindowDelegate;
+    pub struct WindowDelegate {
+        id: IvarEncode<NSInteger, "_window_id">,
+    }
+
+    mod ivars;
 
     unsafe impl ClassType for WindowDelegate {
         #[inherits(NSObject)]
@@ -79,25 +90,136 @@ declare_class!(
     }
 
     unsafe impl WindowDelegate {
-        #[method(init)]
-        unsafe fn init(this: *mut Self) -> Option<NonNull<Self>> {
-            unsafe { msg_send![super(this), init] }
+        #[method(initWithId:)]
+        unsafe fn init_with_id(
+            this: *mut Self,
+            window_id: NSInteger,
+        ) -> Option<NonNull<Self>> {
+            let this: Option<&mut Self> =
+                unsafe { msg_send![super(this), init] };
+
+            if let Some(this) = this {
+                Ivar::write(&mut this.id, window_id);
+                NonNull::new(addr_of_mut!(*this))
+            } else {
+                None
+            }
         }
     }
 
     unsafe impl NSObjectProtocol for WindowDelegate {}
 
     unsafe impl NSWindowDelegate for WindowDelegate {
+        #[method(windowShouldClose:)]
+        unsafe fn window_should_close(&self, _sender: &NSWindow) -> bool {
+            self.post_event(WindowEvent::CloseRequested);
+            true
+        }
+
         #[method(windowWillClose:)]
         unsafe fn window_will_close(&self, _sender: &NSNotification) {
-            sys::set_quit_event();
+            self.post_event(WindowEvent::Destroyed);
+        }
+
+        #[method(windowDidMove:)]
+        unsafe fn window_did_move(&self, notification: &NSNotification) {
+            let object = &*notification.object().unwrap();
+            // The "object" field of NSNotification is a pointer to an NSWindow
+            // for this handler, we just have to "upcast" it ourselves. 
+            let window = &*(addr_of!(*object) as *mut NSWindow);
+            let pos = window.frame().origin;
+            self.post_event(
+                WindowEvent::Moved { x: pos.x as _, y: pos.y as _ }
+            );
+        }
+
+        #[method(windowWillResize:toSize:)]
+        unsafe fn window_will_resize(
+            &self, _sender: &NSWindow, frame_size: NSSize
+        ) -> NSSize {
+            self.post_event(
+                WindowEvent::Resized { 
+                    width: frame_size.width as _, 
+                    height: frame_size.height as _ 
+                }
+            );
+            frame_size
+        }
+
+        #[method(windowDidBecomeKey:)]
+        unsafe fn window_did_become_key(&self, _notification: &NSNotification)
+        {
+            self.post_event(WindowEvent::SetFocus);
+            // Until we come up with a better solution, we'll just fire both
+            // SetFocus and Activated here.
+            self.post_event(WindowEvent::Activate);
+        }
+
+        #[method(windowDidResignKey:)]
+        unsafe fn window_did_resign_key(&self, _notification: &NSNotification)
+        {
+            self.post_event(WindowEvent::KillFocus);
+            // Same story here, with KillFocus and Deactivate.
+            self.post_event(WindowEvent::Deactivate);
+        }
+
+        #[method(windowDidChangeScreen:)]
+        unsafe fn window_did_change_screen(
+            &self, notification: &NSNotification
+        ) {
+            let object = &*notification.object().unwrap();
+            // The "object" field of NSNotification is a pointer to an NSWindow
+            // for this handler, we just have to "upcast" it ourselves. 
+            let window = &*(addr_of!(*object) as *mut NSWindow);
+            let screen = window.screen().unwrap();
+            let bits_per_pixel = 
+                NSBitsPerPixelFromDepth(screen.depth()) as u32;
+            let horz_res = screen.frame().size.width as u32;
+            let vert_res = screen.frame().size.height as u32;
+            self.post_event(
+                WindowEvent::DisplayChange { 
+                    bits_per_pixel, horz_res, vert_res 
+                }
+            );
         }
     }
 );
 
 impl WindowDelegate {
-    pub fn new() -> Id<Self> {
-        unsafe { msg_send_id![Self::alloc(), init] }
+    pub fn new(window_id: NSInteger) -> Id<Self> {
+        unsafe { msg_send_id![Self::alloc(), initWithId:window_id] }
+    }
+
+    pub fn window_id(&self) -> NSInteger {
+        *self.id
+    }
+
+    fn make_event(ev: WindowEvent, w_num: NSInteger) -> Id<NSEvent> {
+        let ev = Box::new(ev);
+        unsafe { NSEvent::otherEventWithType_location_modifierFlags_timestamp_windowNumber_context_subtype_data1_data2(
+            NSEventTypeApplicationDefined, CGPoint::new(0.0, 0.0), 0,
+            NSDate::now().timeIntervalSince1970(), w_num, None, 0,
+            Box::into_raw(ev) as _, 0
+        ) }.unwrap()
+    }
+
+    fn post_event(&self, ev: WindowEvent) {
+        unsafe {
+            NSApp.unwrap().postEvent_atStart(
+                &Self::make_event(ev, self.window_id()),
+                false,
+            )
+        };
+    }
+}
+
+pub trait WindowEventExtAppkit {
+    unsafe fn from_nsevent(ev: &NSEvent) -> Self;
+}
+
+impl WindowEventExtAppkit for WindowEvent {
+    unsafe fn from_nsevent(ev: &NSEvent) -> Self {
+        *Box::<Self>::from_raw(ev.data1() as _)
     }
 }
 
@@ -122,6 +244,7 @@ impl TryFrom<KeyCode> for KeyboardScancode {
             0x0D => Ok(KeyboardScancode::W),
             0x0E => Ok(KeyboardScancode::E),
             0x0F => Ok(KeyboardScancode::R),
+
             0x10 => Ok(KeyboardScancode::Y),
             0x11 => Ok(KeyboardScancode::T),
             0x12 => Ok(KeyboardScancode::Key1),
@@ -138,10 +261,12 @@ impl TryFrom<KeyCode> for KeyboardScancode {
             0x1D => Ok(KeyboardScancode::Key0),
             0x1E => Ok(KeyboardScancode::OpenBracket),
             0x1F => Ok(KeyboardScancode::O),
+
             0x20 => Ok(KeyboardScancode::U),
             0x21 => Ok(KeyboardScancode::CloseBracket),
             0x22 => Ok(KeyboardScancode::I),
             0x23 => Ok(KeyboardScancode::P),
+            0x24 => Ok(KeyboardScancode::Enter),
             0x25 => Ok(KeyboardScancode::L),
             0x26 => Ok(KeyboardScancode::J),
             0x27 => Ok(KeyboardScancode::Apostrophe),
@@ -153,14 +278,30 @@ impl TryFrom<KeyCode> for KeyboardScancode {
             0x2D => Ok(KeyboardScancode::N),
             0x2E => Ok(KeyboardScancode::M),
             0x2F => Ok(KeyboardScancode::Period),
+
+            0x30 => Ok(KeyboardScancode::Tab),
+            0x31 => Ok(KeyboardScancode::Space),
             0x32 => Ok(KeyboardScancode::Tilde),
+            0x33 => Ok(KeyboardScancode::Backspace),
+            0x35 => Ok(KeyboardScancode::Esc),
+            0x37 => Ok(KeyboardScancode::LSys),
+            0x38 => Ok(KeyboardScancode::LShift),
+            0x39 => Ok(KeyboardScancode::CapsLk),
+            0x3A => Ok(KeyboardScancode::LAlt),
+            0x3B => Ok(KeyboardScancode::LCtrl),
+            0x3C => Ok(KeyboardScancode::RShift),
+            0x3D => Ok(KeyboardScancode::RAlt),
+            0x3E => Ok(KeyboardScancode::RCtrl),
+            0x3F => Ok(KeyboardScancode::Fn),
             0x41 => Ok(KeyboardScancode::NumPeriod),
+
             0x43 => Ok(KeyboardScancode::NumAsterisk),
             0x45 => Ok(KeyboardScancode::NumPlus),
             0x47 => Ok(KeyboardScancode::NumLk),
             0x4B => Ok(KeyboardScancode::NumSlash),
             0x4C => Ok(KeyboardScancode::NumEnter),
             0x4E => Ok(KeyboardScancode::NumHyphen),
+
             0x52 => Ok(KeyboardScancode::Num0),
             0x53 => Ok(KeyboardScancode::Num1),
             0x55 => Ok(KeyboardScancode::Num3),
@@ -171,18 +312,6 @@ impl TryFrom<KeyCode> for KeyboardScancode {
             0x5B => Ok(KeyboardScancode::Num8),
             0x5C => Ok(KeyboardScancode::Num9),
 
-            0x24 => Ok(KeyboardScancode::Enter),
-            0x30 => Ok(KeyboardScancode::Tab),
-            0x31 => Ok(KeyboardScancode::Space),
-            0x33 => Ok(KeyboardScancode::Backspace),
-            0x35 => Ok(KeyboardScancode::Esc),
-            0x37 => Ok(KeyboardScancode::LSys),
-            0x38 => Ok(KeyboardScancode::LShift),
-            0x39 => Ok(KeyboardScancode::CapsLk),
-            0x3A => Ok(KeyboardScancode::LAlt),
-            0x3B => Ok(KeyboardScancode::LCtrl),
-            0x3C => Ok(KeyboardScancode::RShift),
-            0x3D => Ok(KeyboardScancode::Fn),
             0x60 => Ok(KeyboardScancode::F5),
             0x61 => Ok(KeyboardScancode::F6),
             0x62 => Ok(KeyboardScancode::F7),
@@ -192,6 +321,7 @@ impl TryFrom<KeyCode> for KeyboardScancode {
             0x66 => Ok(KeyboardScancode::F11),
             0x6D => Ok(KeyboardScancode::F10),
             0x6F => Ok(KeyboardScancode::F12),
+
             0x72 => Ok(KeyboardScancode::Insert),
             0x73 => Ok(KeyboardScancode::Home),
             0x74 => Ok(KeyboardScancode::PgUp),
@@ -206,7 +336,7 @@ impl TryFrom<KeyCode> for KeyboardScancode {
             0x7D => Ok(KeyboardScancode::ArrowDown),
             0x7E => Ok(KeyboardScancode::ArrowUp),
 
-            _ => Err(())
+            _ => Err(()),
         }
     }
 }
@@ -215,7 +345,20 @@ impl TryFrom<&NSEvent> for WindowEvent {
     type Error = ();
     fn try_from(value: &NSEvent) -> Result<Self, Self::Error> {
         match unsafe { value.r#type() } {
-            _ => todo!()
+            // Some events can't be caught in our event loop and are only
+            // sent to the window delegate instead. We created handlers above
+            // to catch those events and post them to the event loop ourselves.
+            // They all have a type of [`NSEventTypeApplicationDefined`].
+            NSEventTypeApplicationDefined => {
+                // Sanity check - whenever we post events, subtype and d2 are
+                // always set to 0. If they aren't zero, we're getting events
+                // we shouldn't be getting
+                assert!(unsafe { value.subtype() == 0 && value.data2() == 0 });
+                Ok(unsafe { WindowEvent::from_nsevent(value) })
+            }
+            // Other events, like keyboard and mouse events are sent straight
+            // to the event loop, and we'll translate them here.
+            _ => todo!(),
         }
     }
 }
