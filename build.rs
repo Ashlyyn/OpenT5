9ee9aa42396a82202458fd6fae824cb4b3cc5ded
Ashlyn <ashbtw19937@gmail.com>
2023-08-05 02:08:53 -0600

    fix/add some cfg_aliases, add windows_use_vulkan and macos_use_vulkan features for future use, add functions to get build info

--- build.rs
+++ build.rs
@@ -7,13 +7,23 @@ fn main() {
         // Windows and unix already defined by rust
         macos: { target_os = "macos" },
         linux: { target_os = "linux" },
+        freebsd: { target_os = "freebsd" },
+        openbsd: { target_os = "freebsd" },
+        dragonflybsd: { target_os = "dragonfly" },
+        netbsd: { target_os = "netbsd" },
         bsd: { any(
             target_os = "freebsd", target_os = "dragonfly",
             target_os = "openbsd", target_os = "netbsd"
         ) },
-        other_unix: {
-            all(unix, not(target_os = "macos"), not(target_os = "linux"))
-        },
+        other_unix: { all(
+            unix, 
+            not(target_os = "macos"), 
+            not(target_os = "linux"), 
+            not(any(
+                    target_os = "freebsd", target_os = "dragonfly",
+                    target_os = "openbsd", target_os = "netbsd"
+            ))
+        ) },
         // We'll have to update this if we add support for OSes
         // that aren't Windows or Unix
         // No, this is not the PS3's OtherOS
@@ -22,16 +32,31 @@ fn main() {
         // remotely-supported OS-less platform is wasm
         no_os: { target_arch = "wasm32" },
         // Display servers
-        xlib: { any(other_unix, feature = "linux_use_xlib", feature = "macos_use_xlib") },
+        xlib: { any(
+            other_unix, 
+            all(target_os = "macos", feature = "macos_use_xlib"),
+            all(target_os = "linux", feature = "linux_use_xlib")
+        ) },
         wayland: { all(target_os = "linux", feature = "linux_use_wayland") },
         appkit: { all(target_os = "macos", feature = "macos_use_appkit") },
         // Rendering backends
         d3d9: { all(windows, feature = "windows_use_d3d9") },
-        wgpu: { any(feature = "windows_use_wgpu", feature = "linux_use_wgpu", feature = "macos_use_wgpu") },
+        wgpu: { any(
+            all(windows, feature = "windows_use_wgpu"), 
+            all(target_os = "linux", feature = "linux_use_wgpu"), 
+            all(target_os = "macos", feature = "macos_use_wgpu")
+        ) },
+        metal: { all(target_os = "macos", feature = "macos_use_metal") },
+        vulkan: { any(
+            all(windows, feature = "windows_use_vulkan"), 
+            all(target_os = "macos", feature = "macos_use_vulkan"), 
+            all(target_os = "linux", feature = "linux_use_vulkan")
+        ) },
         // Arches
         x86: { any(target_arch = "x86", target_arch = "x86_64") },
         i686: { target_arch = "x86" },
         x86_64: { target_arch = "x86_64" },
+        aarch64: { target_arch = "aarch64" },
         wasm: { target_arch = "wasm32" },
 
         native: { not(target_arch = "wasm32") },
