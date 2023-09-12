b26cdede47c8d651f020a2bcf4a2e8aa1365d821
Ashlyn <ashbtw19937@gmail.com>
2023-09-12 04:19:29 -0600

    fix implementations of get_os_folder_path, remove UserConfig and Home from OsFolder (never used), remove pub where unnecessary, document Fd, implement more fs fns

--- engine/src/fs.rs
+++ engine/src/fs.rs
@@ -8,7 +8,6 @@ use cfg_if::cfg_if;
 use core::{str::FromStr, sync::atomic::AtomicUsize};
 use std::{
     ffi::OsStr,
-    fs::OpenOptions,
     io::{Read, Write},
     path::{Path, PathBuf},
     sync::RwLock,
@@ -20,10 +19,9 @@ cfg_if! {
         use windows::Win32::{
             UI::Shell::{
                 SHGetFolderPathA, CSIDL_LOCAL_APPDATA, CSIDL_FLAG_CREATE,
-                CSIDL_MYDOCUMENTS, CSIDL_PROFILE, SHGFP_TYPE_CURRENT,
+                CSIDL_PERSONAL, SHGFP_TYPE_CURRENT,
             },
             Foundation::MAX_PATH};
-            use core::ffi::CStr;
             use core::mem::transmute;
     }
 }
@@ -34,15 +32,14 @@ const MAX_PATH_LEN: usize = MAX_PATH as usize;
 #[cfg(unix)]
 const MAX_PATH_LEN: usize = 4096usize;
 
+// Probably a safe value to default to.
 #[cfg(not(any(windows, unix)))]
 const MAX_PATH_LEN: usize = 256usize;
 
 #[derive(Copy, Clone, Debug)]
 pub enum OsFolder {
-    UserConfig,
     UserData,
     Documents,
-    Home,
 }
 
 cfg_if! {
@@ -53,12 +50,10 @@ cfg_if! {
             clippy::indexing_slicing,
             clippy::multiple_unsafe_ops_per_block
         )]
-        pub fn get_os_folder_path(os_folder: OsFolder) -> Option<String> {
+        pub fn get_os_folder_path(os_folder: OsFolder) -> Option<PathBuf> {
             let csidl: u32 = match os_folder {
-                OsFolder::UserData
-                    | OsFolder::UserConfig => CSIDL_LOCAL_APPDATA,
-                OsFolder::Documents => CSIDL_MYDOCUMENTS,
-                OsFolder::Home => CSIDL_PROFILE,
+                OsFolder::UserData => CSIDL_LOCAL_APPDATA,
+                OsFolder::Documents => CSIDL_PERSONAL,
             };
 
             let mut buf: [u8; MAX_PATH as usize] = [0; MAX_PATH as usize];
@@ -80,37 +75,30 @@ cfg_if! {
                     // Null-terminate the string, in case the folder path
                     // was exactly MAX_PATH characters.
                     buf[buf.len() - 1] = 0x00;
-                    let Ok(c) = CStr::from_bytes_until_nul(&buf) else {
-                        return None
-                    };
-                    Some(c.to_str().unwrap().to_owned())
+                    Some(PathBuf::new().join(unsafe { OsStr::from_os_str_bytes_unchecked(&buf) }).join("Activision").join("CoD"))
                 },
                 Err(_) => None,
             }
         }
     } else if #[cfg(target_family = "unix")] {
         #[allow(clippy::needless_pass_by_value)]
-        pub fn get_os_folder_path(os_folder: OsFolder) -> Option<String> {
+        pub fn get_os_folder_path(os_folder: OsFolder) -> Option<PathBuf> {
             let envar = match os_folder {
                 OsFolder::UserData => "XDG_DATA_HOME",
-                OsFolder::UserConfig => "XDG_CONFIG_HOME",
                 OsFolder::Documents => "XDG_DOCUMENTS_DIR",
-                OsFolder::Home => "HOME",
             };
 
             let Ok(home) = std::env::var("HOME") else { return None };
 
             let envar_default = match os_folder {
                 OsFolder::UserData => format!("{}/.local/share", home),
-                OsFolder::UserConfig => format!("{}/.config", home),
                 OsFolder::Documents => format!("{}/Documents", home),
-                OsFolder::Home => home,
             };
 
-            Some(std::env::var(envar).map_or(envar_default, |s| s))
+            Some(PathBuf::from_str(&std::env::var(envar).map_or(envar_default, |s| s)).unwrap().join("Activision").join("CoD"))
         }
     } else {
-        pub fn get_os_folder_path(os_folder: OsFolder) -> Option<String> {
+        pub fn get_os_folder_path(os_folder: OsFolder) -> Option<PathBuf> {
             compile_error!(
                 "get_os_folder_path unimplemented for OS or arch",
             );
@@ -118,9 +106,7 @@ cfg_if! {
     }
 }
 
-// TODO - Will panic if `path` contains invalid UTF-8 characters.
-// Fix at some point.
-pub fn create_path<P: AsRef<Path>>(path: P) -> Result<PathBuf, std::io::Error> {
+pub fn create_path(path: impl AsRef<Path>) -> Result<PathBuf, std::io::Error> {
     let path = path.as_ref();
 
     if path.is_relative() {
@@ -133,11 +119,11 @@ pub fn create_path<P: AsRef<Path>>(path: P) -> Result<PathBuf, std::io::Error> {
     }
 
     if path.exists() {
-        return Ok(PathBuf::from_str(path.to_str().unwrap()).unwrap());
+        return Ok(path.to_path_buf());
     }
 
     if std::fs::File::create(path).is_ok() {
-        Ok(PathBuf::from_str(path.to_str().unwrap()).unwrap())
+        Ok(path.to_path_buf())
     } else {
         let Some(dir_path) = path.parent() else {
             return Err(std::io::ErrorKind::InvalidFilename.into());
@@ -154,6 +140,7 @@ pub fn create_path<P: AsRef<Path>>(path: P) -> Result<PathBuf, std::io::Error> {
 
 // TODO - fully implement
 pub fn init_filesystem(dev: bool) {
+    // FSH.write().unwrap().fill(None);
     startup("main", dev);
 }
 
@@ -175,7 +162,8 @@ fn register_dvars() {
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
-pub enum Thread {
+#[repr(i32)]
+enum Thread {
     Main,
     Stream,
     Database,
@@ -183,11 +171,18 @@ pub enum Thread {
     Server,
 }
 
+impl Thread {
+    #[must_use]
+    fn as_i32(self) -> i32 {
+        self as i32
+    }
+}
+
 lazy_static! {
     static ref FS_GAMEDIR: RwLock<PathBuf> = RwLock::new(PathBuf::new());
 }
 
-pub fn build_os_path_for_thread(
+fn build_os_path_for_thread(
     base: impl AsRef<Path>,
     gamedir: Option<impl AsRef<Path>>,
     qpath: impl AsRef<Path>,
@@ -227,7 +222,7 @@ pub fn build_os_path(
     build_os_path_for_thread(base, gamedir, qpath, Thread::Main)
 }
 
-pub fn delete(filename: impl AsRef<Path>) -> Result<(), std::io::Error> {
+fn delete(filename: impl AsRef<Path>) -> Result<(), std::io::Error> {
     if filename.as_ref() == &PathBuf::new() {
         return Err(std::io::ErrorKind::InvalidFilename.into());
     }
@@ -241,7 +236,7 @@ pub fn delete(filename: impl AsRef<Path>) -> Result<(), std::io::Error> {
     std::fs::remove_file(ospath)
 }
 
-pub fn get_current_thread() -> Option<Thread> {
+fn get_current_thread() -> Option<Thread> {
     if sys::is_main_thread() {
         Some(Thread::Main)
     } else if sys::is_database_thread() {
@@ -257,9 +252,9 @@ pub fn get_current_thread() -> Option<Thread> {
     }
 }
 
-pub type Iwd = ZipArchive<std::fs::File>;
+type Iwd = ZipArchive<std::fs::File>;
 
-pub enum Qfile {
+enum Qfile {
     ZipFile {
         archive: Arc<RwLock<Iwd>>,
         // Have to embed this here to make our implementation of Read work.
@@ -307,19 +302,43 @@ unsafe impl Send for FileHandleData {}
 
 lazy_static! {
     static ref FSH: RwLock<ArrayVec<Option<FileHandleData>, 70>> =
-        RwLock::new(ArrayVec::new());
+        RwLock::new(ArrayVec::new_const());
 }
 
-#[derive(Copy, Clone, Debug)]
+// Fd is neither [`Copy`] nor [`Clone`] so that our Drop implementation can
+// ensure it's safe to set its corresponding element in FSH to [`None`]
+// (wouldn't be able to do that if copies/clones of the Fd were floating
+// around).
+//
+// We could also implement it as Fd(Arc<u8>) to make it [`Clone`] and then only
+// clear it from FSH when the [`Arc`]'s refcount is 0, but that seems like a
+// lot of overhead for something that shouldn't ever really *need* to be cloned
+
+/// An opaque file handle used by functions in this module.
+///
+/// [`Fd`] is intentionally not [`Copy`] or [`Clone`] to ensure unique
+/// ownership, and there is no way to retieve the underlying [`File`] it
+/// represents.
+///
+/// It's only use is to be passed between functions in this module.
+/// It can only be created by functions in this module, and it will
+/// automatically clean up the resources it uses on [`Drop`].
+#[derive(Debug)]
 pub struct Fd(u8);
 
 impl Fd {
-    pub fn as_usize(self) -> usize {
+    fn as_usize(&self) -> usize {
         self.0 as usize
     }
 }
 
-pub fn handle_for_file(thread: Thread) -> std::io::Result<Fd> {
+impl Drop for Fd {
+    fn drop(&mut self) {
+        FSH.write().unwrap()[self.as_usize()] = None;
+    }
+}
+
+fn handle_for_file(thread: Thread) -> std::io::Result<Fd> {
     let fd_range = match thread {
         Thread::Main => {
             assert!(sys::is_main_thread());
@@ -343,13 +362,47 @@ pub fn handle_for_file(thread: Thread) -> std::io::Result<Fd> {
         }
     };
 
-    for fd in fd_range {
+    for fd in fd_range.clone() {
         if FSH.read().unwrap()[fd].is_none() {
             return Ok(Fd(fd as _));
         }
     }
 
-    todo!()
+    com::warnln!(
+        10.into(),
+        "FILE {:2}: '{}'",
+        fd_range.start(),
+        FSH.read().unwrap()[*fd_range.start()]
+            .as_ref()
+            .unwrap()
+            .name()
+            .to_string_lossy()
+    );
+    com::warnln!(
+        10.into(),
+        "fs::handle_for_file: none free ({})",
+        thread.as_i32()
+    );
+
+    for (i, f) in FSH.read().unwrap().iter().enumerate() {
+        com::println!(
+            10.into(),
+            "FILE {:2}: '{}'",
+            i,
+            f.as_ref().unwrap().name().to_string_lossy()
+        );
+    }
+
+    com::errorln!(com::ErrorParm::DROP, "\x15fs::handle_for_file: none free");
+
+    Err(std::io::ErrorKind::Other.into())
+}
+
+fn handle_for_file_current_thread() -> std::io::Result<Fd> {
+    handle_for_file(
+        get_current_thread()
+            .expect("Does the fs need to support a new thread?"),
+    )
 }
 
 struct Directory {
@@ -585,10 +638,7 @@ pub fn file_read(
     file.read(data)
 }
 
-pub fn file_write(
-    file: &mut impl Write,
-    data: &[u8],
-) -> std::io::Result<usize> {
+fn file_write(file: &mut impl Write, data: &[u8]) -> std::io::Result<usize> {
     file.write(data)
 }
 
@@ -596,7 +646,8 @@ lazy_static! {
     static ref FS_NUM_SERVER_IWDS: AtomicUsize = AtomicUsize::new(0);
     static ref FS_FAKE_CHK_SUM: AtomicUsize = AtomicUsize::new(0);
 }
-pub fn open_file_read_for_thread(
+
+fn open_file_read_for_thread(
     filename: impl AsRef<Path>,
     thread: Thread,
 ) -> Result<(Fd, u64), std::io::Error> {
@@ -769,19 +820,73 @@ pub fn open_file_read_current_thread(
     }
 }
 
+pub fn open_file_read(
+    filename: impl AsRef<Path>,
+) -> std::io::Result<(Fd, u64)> {
+    com::file_accessed().store_relaxed(1);
+    open_file_read_current_thread(filename)
+}
+
+pub fn open_file_append(filename: impl AsRef<Path>) -> std::io::Result<Fd> {
+    let ospath = build_os_path(
+        dvar::get_string("fs_homepath").unwrap(),
+        Some(&*FS_GAMEDIR.read().unwrap()),
+        &filename,
+    );
+    if dvar::get_int("fs_debug").unwrap() != 0 {
+        com::println!(
+            10.into(),
+            "fs::open_file_append: {}",
+            ospath.to_string_lossy()
+        );
+    }
+    let path = create_path(ospath)?;
+    let file = file_open_append(path)?;
+    let fd = handle_for_file_current_thread()?;
+
+    let mut fsh = FSH.write().unwrap();
+    let fh = &mut fsh[fd.as_usize()];
+    *fh = Some(FileHandleData {
+        file: Qfile::File {
+            file,
+            name: filename.as_ref().to_path_buf(),
+        },
+        handle_sync: false,
+        file_size: 0,
+        streamed: false,
+    });
+
+    Ok(fd)
+}
+
 fn file_open_read(
     filename: impl AsRef<Path>,
 ) -> std::io::Result<std::fs::File> {
-    OpenOptions::new().read(true).create(false).open(filename)
+    std::fs::File::options()
+        .read(true)
+        .create(false)
+        .open(filename)
 }
 
 fn file_open_write(
     filename: impl AsRef<Path>,
 ) -> std::io::Result<std::fs::File> {
-    OpenOptions::new().write(true).create(true).open(filename)
+    std::fs::File::options()
+        .write(true)
+        .create(true)
+        .open(filename)
 }
 
-pub fn get_handle_and_open_file(
+fn file_open_append(
+    filename: impl AsRef<Path>,
+) -> std::io::Result<std::fs::File> {
+    std::fs::File::options()
+        .append(true)
+        .create(true)
+        .open(filename)
+}
+
+fn get_handle_and_open_file(
     qpath: impl AsRef<Path>,
     filename: impl AsRef<Path>,
     thread: Thread,
@@ -804,7 +909,7 @@ pub fn get_handle_and_open_file(
     Ok(handle)
 }
 
-pub fn open_file_write_to_dir_for_thread(
+fn open_file_write_to_dir_for_thread(
     qpath: impl AsRef<Path>,
     gamedir: Option<impl AsRef<Path>>,
     thread: Thread,
@@ -834,7 +939,40 @@ pub fn open_file_write(filename: impl AsRef<Path>) -> std::io::Result<Fd> {
     )
 }
 
-pub fn read(fd: Fd) -> Result<Vec<u8>, std::io::Error> {
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum Mode {
+    Read,
+    Write,
+    Append,
+    AppendSync,
+}
+
+pub fn open_file_by_mode(
+    filename: impl AsRef<Path>,
+    mode: Mode,
+) -> std::io::Result<(Fd, usize)> {
+    let r = match mode {
+        Mode::Read => {
+            open_file_read(filename).map(|(fd, size)| (fd, size as usize))
+        }
+        Mode::Write => open_file_write(filename).map(|fd| (fd, 0usize)),
+        Mode::Append | Mode::AppendSync => {
+            open_file_append(filename).map(|fd| (fd, 0usize))
+        }
+    };
+
+    if let Ok((ref fd, file_size)) = r {
+        let mut fsh = FSH.write().unwrap();
+        let fh = fsh[fd.as_usize()].as_mut().unwrap();
+        fh.file_size = file_size;
+        fh.streamed = false;
+        fh.handle_sync = mode == Mode::AppendSync;
+    }
+
+    r
+}
+
+pub fn read(fd: &Fd) -> Result<Vec<u8>, std::io::Error> {
     let mut buf = Vec::new();
     FSH.write().unwrap()[fd.as_usize()]
         .as_mut()
@@ -844,7 +982,7 @@ pub fn read(fd: Fd) -> Result<Vec<u8>, std::io::Error> {
     Ok(buf)
 }
 
-pub fn write(fd: Fd, data: &[u8]) -> std::io::Result<()> {
+pub fn write(fd: &Fd, data: &[u8]) -> std::io::Result<()> {
     let mut fsh = FSH.write().unwrap();
     let fh = &mut fsh[fd.as_usize()].as_mut().unwrap();
     match fh.file {
@@ -874,14 +1012,14 @@ pub fn read_file(path: &Path) -> Result<Vec<u8>, std::io::Error> {
     }
     let (fd, _) = open_file_read_current_thread(path)?;
     FS_LOADSTACK.increment_wrapping();
-    read(fd)
+    read(&fd)
 }
 
 pub fn write_file(path: impl AsRef<Path>, data: &[u8]) -> std::io::Result<()> {
     assert_ne!(path.as_ref(), Path::new(""));
 
     if let Ok(fd) = open_file_write(&path) {
-        let r = write(fd, data);
+        let r = write(&fd, data);
         if r.is_err() {
             delete(&path)?;
         }
